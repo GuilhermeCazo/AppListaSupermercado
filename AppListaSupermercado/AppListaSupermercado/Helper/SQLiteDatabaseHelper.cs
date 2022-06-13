e375f158f8750345928de6a582631582d576d153
Usuario <Usuario@CPD02-M16>
2022-06-13 09:11:26 -0300

    dfwseqweeeeeeeeeeeeeeeeeeeeeeee

--- AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
+++ AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
@@ -19,29 +19,41 @@ namespace AppListaSupermercado.Helper
             _conn = new SQLiteAsyncConnection(path);
             _conn.CreateTableAsync<Produto>().Wait();
         }
-        public void insert(Produto p) //PAREI AQUI, =25:46
-        {
 
+        public Task<int> Insert(Produto p) //PAREI AQUI, =25:46
+        {
+            return _conn.InsertAsync(p);
         }
 
-        public void update(Produto p)
+
+
+        public void Update(Produto p)
         {
 
+            String sql = "UPDATE Produto SET NomeProduto=?, Quantidade=?, PrecoEstimado=?, PrecoPago=? WHERE id=?";
+
+            _conn.QueryAsync<Produto>(sql, p.NomeProduto, p.Quantidade, p.PrecoEstimado, p.PrecoEstimado, p.Id);
         }
 
+
+        /**
         public Task<Produto> getById(int id)
         {
             return new Produto();
         }
+        **/
 
-        public Task<List<Produto>> getAll()
+
+        public Task<List<Produto>> GetAll()
         {
-
+            return _conn.Table<Produto>().ToListAsync();
         }
 
-        public void delete(int id)
+        /**
+        public Task<int> Delete(int id)
         {
-
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
+        **/
     }
 }
