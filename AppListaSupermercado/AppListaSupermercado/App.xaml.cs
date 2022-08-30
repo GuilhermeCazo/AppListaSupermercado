233d2a0d89a9dc0dd89f341710ed46fdff82031c
Usuario <Usuario@CPD02-M01>
2022-08-30 12:22:53 -0300

    salvando e listando os dados dos produtos, adiçãio do remover e da programacao da listagem

--- AppListaSupermercado/AppListaSupermercado/App.xaml.cs
+++ AppListaSupermercado/AppListaSupermercado/App.xaml.cs
@@ -2,15 +2,40 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
+using AppListaSupermercado.Helper;
+using System.IO;
+
 namespace AppListaSupermercado
 {
     public partial class App : Application
     {
+
+        static SQLiteDatabaseHelper database;
+
+        public static SQLiteDatabaseHelper Database
+        {
+
+            get 
+            { 
+                if (database == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "arquivo.db3");
+
+                    database = new SQLiteDatabaseHelper(path);
+                }
+
+                return database;
+
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new View.ListaProdutos());
+
         }
 
         protected override void OnStart()
--- AppListaSupermercado/AppListaSupermercado/View/ListaProdutos.xaml.cs
+++ AppListaSupermercado/AppListaSupermercado/View/ListaProdutos.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AppListaSupermercado.Model;
+
+using System.Collections.ObjectModel;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +16,130 @@ namespace AppListaSupermercado.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListaProdutos : ContentPage
     {
+
+
+        ObservableCollection<Produto> lista_produtos = new ObservableCollection<Produto>();
+
+
         public ListaProdutos()
         {
+
             InitializeComponent();
+
+            lst_produtos.ItemsSource = lista_produtos;
+
         }
+
+        
+
+        /*
+         private void ToolbarItem_Clicked_Novo(object sender, EventArgs e)
+          {
+            try
+            {
+               Navigation.PushAsync(new FormularioCadastro());
+        
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+           }
+          }
+
+
+        //===============================================================//
+
+           O VALOR TOTAL, MAS VOU TER QUE FAZER DIFERENTE
+        
+            private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
+              {
+        
+         double soma = lista_produtos.Sum(i => i.Preco_Unico * i.Quantidade)
+          string msg = "O total da compra é" + soma;
+        
+         DisplayAlert("Ops", msg, "ok");
+         }
+
+        */
+
+
+        protected override void OnAppearing()
+        {
+            if (lista_produtos.Count == 0)
+            {
+                System.Threading.Tasks.Task.Run(async () =>
+                {
+
+                    List<Produto> temp = await App.Database.GetAll();
+
+                    foreach (Produto item in temp)
+                    {
+                        lista_produtos.Add(item);
+                    }
+
+                    ref_carregando.IsRefreshing = false;
+                });
+
+            }
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            
+            MenuItem disparador = (MenuItem)sender;
+
+
+            
+            Produto produto_selecionado = (Produto)disparador.BindingContext;
+
+            
+            bool confirmacao = await DisplayAlert("Tem Certeza?", "Remover Item?", "Sim", "Não");
+
+            if (confirmacao)
+            {
+                await App.Database.Delete(produto_selecionado.Id);
+
+                
+                lista_produtos.Remove(produto_selecionado);
+            }
+        }
+
+
+
+        //busca
+        private void txt_busca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            string buscou = e.NewTextValue;
+
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                List<Produto> temp = await App.Database.Search(buscou);
+
+                
+                lista_produtos.Clear();
+
+                foreach (Produto item in temp)
+                {
+                    lista_produtos.Add(item);
+                }
+
+                ref_carregando.IsRefreshing = false;
+            });
+        }
+
+
+      //editar (vejo bunitinho depois, não sei se da certo pq tenho que fazer o arquivo bonitinho
+
+        /*
+        private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            
+            Navigation.PushAsync(new EditarProduto
+            {
+                BindingContext = (Produto)e.SelectedItem
+            });
+        }
+        */
 
         private void Btn_FormCadastro(object sender, EventArgs e)
         {
